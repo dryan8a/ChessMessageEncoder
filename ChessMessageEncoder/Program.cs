b7a1d1fb46b755942012ddb4e9cee5d31e514b1f
Dylan Ochoa <Dylan.Ochoa@GMR.local>
2019-06-07 17:34:29 -0700

    I thought it works but it doesn't so cry and its all because the generate all moves function somehow acts differently in the decoder than the encoder and doesn't generate stupid Nxf3 during the decoding I checked all the pieces and previous moves and yet its still different the thing to copy from is on documents and I wrote this in a run-on on purpose

--- ChessMessageEncoder/Program.cs
+++ ChessMessageEncoder/Program.cs
@@ -8,6 +8,133 @@ namespace ChessMessageEncoder
 {
     class Program
     {
+
+        public static string Encode(string message)
+        {
+            var board = new ChessBoard();
+            string Output = "";
+            string Input = message;
+            byte[] bytes = Encoding.UTF8.GetBytes(Input);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                Console.Write(bytes[i] + " ");
+            }
+            Array.Reverse(bytes);
+            BigInteger correspondingNum = (BigInteger)Math.Pow(256, bytes.Length);
+            for (int i = 1; i <= bytes.Length; i++)
+            {
+                correspondingNum += bytes[i - 1] * (int)Math.Pow(256, bytes.Length - i);
+            }
+            Console.WriteLine("");
+            Console.WriteLine(correspondingNum);
+            Console.WriteLine("");
+            int round = 0;
+            while (correspondingNum != 0)
+            {
+                if (board.isWhitesTurn)
+                {
+                    round++;
+                    Output += round.ToString() + ". ";
+                }
+                var moves = board.GetAllPossibleMoves();
+                string tempExecutedMove = moves[(int)(correspondingNum % moves.Count)].Substring(moves[(int)(correspondingNum % moves.Count)].IndexOf("~") + 1);
+                Output += tempExecutedMove + " ";
+                board.ExecuteMoves(moves[(int)(correspondingNum % moves.Count)].Substring(0, moves[(int)(correspondingNum % moves.Count)].IndexOf("~")), tempExecutedMove);
+                correspondingNum /= moves.Count;
+            }
+            if (board.isWhitesTurn)
+            {
+                Output += "{ White resigns. } 0-1";
+            }
+            else
+            {
+                Output += "{ Black resigns. } 1-0";
+            }
+            return Output;
+        }
+
+        public static string Decode(string encodedGame)
+        {
+            /* take leftover number (0), multiply by what you divided by
+                     * add the modded number, multiply by by what you diveded by to get that number
+                     * repeat until you have no more numbers to multiply by
+                     * check this with the JS example online, you'll get it then
+                    */
+            var board = new ChessBoard();
+            encodedGame = (encodedGame.Remove(encodedGame.Length - 22)).Trim();
+            List<int> indeces = new List<int>();
+            List<int> amountsOfPossibleMoves = new List<int>();
+            while (encodedGame.Length != 0)
+            {
+                if (board.isWhitesTurn && encodedGame.Length > 3)
+                {
+                    encodedGame = encodedGame.Remove(0, 3).Trim();
+                }
+                var moves = board.GetAllPossibleMoves();
+                string correspondingStringToMoves = "";
+                for (int i = 0; i < moves.Count; i++)
+                {
+                    correspondingStringToMoves += (moves[i] + "|");
+                }
+                amountsOfPossibleMoves.Add(moves.Count);
+
+                int amountOfCharsToRemove = 3;
+                if (encodedGame[0] == 'K' || encodedGame[0] == 'Q' || encodedGame[0] == 'N' || encodedGame[0] == 'B' || encodedGame[0] == 'R')
+                {
+                    amountOfCharsToRemove++;
+                }
+                if (encodedGame[1] == 'x')
+                {
+                    amountOfCharsToRemove++;
+                }
+                correspondingStringToMoves = correspondingStringToMoves.Remove(correspondingStringToMoves.IndexOf(encodedGame.Substring(0, amountOfCharsToRemove - 1)) + amountOfCharsToRemove - 1);
+                correspondingStringToMoves = correspondingStringToMoves.Substring(correspondingStringToMoves.LastIndexOf('|') + 1);
+                indeces.Add(moves.IndexOf(correspondingStringToMoves));
+                if (encodedGame.Length < amountOfCharsToRemove)
+                {
+                    encodedGame = "";
+                }
+                else
+                {
+                    encodedGame = encodedGame.Remove(0, amountOfCharsToRemove).Trim();                    
+                }
+                board.ExecuteMoves(correspondingStringToMoves.Substring(0, correspondingStringToMoves.IndexOf('~')), correspondingStringToMoves.Substring(correspondingStringToMoves.IndexOf('~') + 1));
+
+            }
+            BigInteger correspondingNum = 0;
+            for (int i = indeces.Count - 1; i > 0; i--)
+            {
+                correspondingNum += indeces[i];
+                correspondingNum *= amountsOfPossibleMoves[i - 1];
+            }
+            correspondingNum += indeces[0];
+            Console.WriteLine(correspondingNum);
+            bool foundPower = false;
+            int power = 0;
+            while (!foundPower)
+            {
+                if (correspondingNum - (BigInteger)Math.Pow(256, power) > 0)
+                {
+                    power++;
+                }
+                else
+                {
+                    power--;
+                    foundPower = true;
+                }
+            }
+            byte[] bytes = new byte[power];
+            correspondingNum -= (BigInteger)Math.Pow(256, power);
+            for (int i = power - 1; i > -1; i--)
+            {
+                int temp = (int)(correspondingNum / (BigInteger)Math.Pow(256, i));
+                correspondingNum -= (BigInteger)(Math.Pow(256, i) * temp);
+                bytes[i] = (byte)temp;
+                Console.Write(bytes[i] + " ");
+            }
+            return Encoding.UTF8.GetString(bytes);
+        }
+
         static void Main(string[] args)
         {
             while (true)
@@ -18,99 +145,17 @@ namespace ChessMessageEncoder
                 action = action.Trim();
                 if (action == "1" || action == "encode" || action == "Encode" || action == "e" || action == "E")
                 {
-                    var board = new ChessBoard();
-                    string Output = "";
-                    string Input = Console.ReadLine();
-                    byte[] bytes = Encoding.UTF8.GetBytes(Input);
-                    for (int i = 0; i < bytes.Length; i++)
-                    {
-                        Console.Write(bytes[i] + " ");
-                    }
-                    Array.Reverse(bytes);
-                    BigInteger correspondingNum = (BigInteger)Math.Pow(256, bytes.Length);
-                    for (int i = 1; i <= bytes.Length; i++)
-                    {
-                        correspondingNum += bytes[i - 1] * (int)Math.Pow(256, bytes.Length - i);
-                    }
-                    Console.WriteLine("");
-                    Console.WriteLine(correspondingNum);
-                    Console.WriteLine("");
-                    int round = 0;
-                    while (correspondingNum != 0)
-                    {
-                        if (board.isWhitesTurn)
-                        {
-                            round++;
-                            Output += round.ToString() + ". ";
-                        }
-                        var moves = board.GetAllPossibleMoves();
-                        string tempExecutedMove = moves[(int)(correspondingNum % moves.Count)].Substring(moves[(int)(correspondingNum % moves.Count)].IndexOf("~") + 1);
-                        Output += tempExecutedMove + " ";
-                        board.ExecuteMoves(moves[(int)(correspondingNum % moves.Count)].Substring(0, moves[(int)(correspondingNum % moves.Count)].IndexOf("~")), tempExecutedMove);
-                        correspondingNum /= moves.Count;
-                    }
-                    if (board.isWhitesTurn)
-                    {
-                        Output += "{ White resigns. } 0-1";
-                    }
-                    else
-                    {
-                        Output += "{ Black resigns. } 1-0";
-                    }
-                    Console.WriteLine(Output);
+                    Console.WriteLine(Encode(Console.ReadLine()));
                     Console.ReadLine();
                     Console.Clear();
                 }
                 else if(action == "2" || action == "Decode" || action == "decode" || action == "d" || action == "D")
                 {
-                    /* take leftover number (0), multiply by what you divided by
-                     * add the modded number, multiply by by what you diveded by to get that number
-                     * repeat until you have no more numbers to multiply by
-                     * check this with the JS example online, you'll get it then
-                    */ 
-                    var board = new ChessBoard();
-                    string encodedGame = Console.ReadLine();
-                    encodedGame = (encodedGame.Remove(encodedGame.Length - 22)).Trim();
-                    List<int> indeces = new List<int>();
-                    List<int> amountsOfPossibleMoves = new List<int>();
-                    while(encodedGame.Length != 0)
-                    {
-                        if(board.isWhitesTurn)
-                        {
-                            encodedGame = encodedGame.Remove(0, 3);
-                        }
-                        var moves = board.GetAllPossibleMoves();
-                        string correspondingStringToMoves = "";
-                        for(int i = 0;i<moves.Count;i++)
-                        {
-                            correspondingStringToMoves += (moves[i] + "|");
-                        }
-                        amountsOfPossibleMoves.Add(moves.Count);
-
-                        int amountOfCharsToRemove = 3;
-                        if(encodedGame[0] == 'K' || encodedGame[0] == 'Q' || encodedGame[0] == 'N' || encodedGame[0] == 'B' || encodedGame[0] == 'R')
-                        {
-                            amountOfCharsToRemove++;
-                        }
-                        if(encodedGame[1] == 'x')
-                        {
-                            amountOfCharsToRemove++;
-                        }
-
-                        correspondingStringToMoves = correspondingStringToMoves.Remove(correspondingStringToMoves.IndexOf(encodedGame.Substring(0, amountOfCharsToRemove - 1)) + amountOfCharsToRemove - 1);
-                        //indeces.Add(moves.IndexOf());
-                        encodedGame = encodedGame.Remove(0, amountOfCharsToRemove);
-                       
-                    }
-                    BigInteger correspondingNum = 0;
-                    for(int i = 0;i<indeces.Count-1;i++)
-                    {
-                        correspondingNum += indeces[i];
-                        correspondingNum *= amountsOfPossibleMoves[i + 1];
-                    }
-                    correspondingNum += indeces[indeces.Count - 1];
-                    Console.WriteLine(correspondingNum);
+                    var a = Decode(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine(a);
                     Console.ReadLine();
+                    Console.Clear();
                 }
             }
         }
@@ -391,7 +436,7 @@ namespace ChessMessageEncoder
             {
                 while (true)
                 {
-                    if (abMoves.IndexOf(currentPos[0]) + moveChange < abMoves.Count)
+                    if (abMoves.IndexOf(currentPos[0]) + moveChange < abMoves.Count && abMoves.IndexOf(currentPos[0]) + moveChange > 0)
                     {
                         currentPos = "" + abMoves[abMoves.IndexOf(currentPos[0]) + moveChange].ToString() + currentPos[1];
                     }
